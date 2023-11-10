da687ed980c9e894512753f70469dee008a87872
chinedu2040 <udezechinedu2040@gmail.com>
2023-11-10 05:24:33 +0100

    manual cors setup

--- app.js
+++ app.js
@@ -17,7 +17,12 @@ dotenv.config(); // Load environment variables from .env
 const app = express();
 
 app.use(cors());
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://busy-blue-gharial.cyclic.app'); // Replace with your client's origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 // Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
