38775136a6e9dc752349e50dee68787da7a4c427
chinedu2040 <udezechinedu2040@gmail.com>
2023-11-12 02:29:13 +0100

    modiified app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// app.js
 const path = require('path');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
@@ -6,43 +5,31 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-dotenv.config(); // Load environment variables from .env
 
+dotenv.config(); // Load environment variables from .env
 
 const app = express();
 
-app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://busy-blue-gharial.cyclic.app'); // Replace with your client's origin
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-// Development logging
-if (process.env.NODE_ENV === 'production') {
-  app.use(morgan('prod'));
-}
+// Enable trust proxy to correctly handle 'X-Forwarded-For' header
+app.set('trust proxy', 1);
 
+// CORS setup
+const allowedOrigin = 'https://busy-blue-gharial.cyclic.app'; // Replace with your client's origin
+app.use(cors({
+  origin: allowedOrigin,
+  methods: 'GET, POST, PUT, DELETE',
+  credentials: true,
+}));
 
-// Middleware to parse JSON
-app.use(express.json());
-
-// Middleware to parse cookies
-app.use(cookieParser());
 // Set security HTTP headers
 app.use(helmet());
 
-// Development logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-// Limit requests from same API
+// Limit requests from the same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -50,10 +37,14 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-// Body parser, reading data from body into req.body
+// Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
-app.use(cookieParser());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -82,6 +73,8 @@ app.use((req, res, next) => {
   next();
 });
 
+// Middleware to parse cookies
+app.use(cookieParser());
 
 // Connect to MongoDB using the environment variable
 const dbURL = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
@@ -103,10 +96,12 @@ const AppError = require('./utils/appError');
 const tourRoutes = require('./routes/tourRoutes');
 const userRoutes = require('./routes/userRoutes');
 const globalErrorHandler = require('./controllers/errorController');
+
 // Use Routes
 app.use('/api/tour', tourRoutes);
 app.use('/api/users', userRoutes);
 
+// Error handling for unmatched routes
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
@@ -117,3 +112,123 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
 });
+
+
+
+
+
+// // app.js
+// const path = require('path');
+// const rateLimit = require('express-rate-limit');
+// const helmet = require('helmet');
+// const mongoSanitize = require('express-mongo-sanitize');
+// const xss = require('xss-clean');
+// const hpp = require('hpp');
+// const express = require('express');
+// const cors = require('cors')
+// const morgan = require('morgan');
+// const cookieParser = require('cookie-parser');
+// const dotenv = require('dotenv');
+// const mongoose = require('mongoose');
+// dotenv.config(); // Load environment variables from .env
+
+
+// const app = express();
+
+// app.use(cors());
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://busy-blue-gharial.cyclic.app'); // Replace with your client's origin
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
+// // Development logging
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(morgan('prod'));
+// }
+
+
+// // Middleware to parse JSON
+// app.use(express.json());
+
+// // Middleware to parse cookies
+// app.use(cookieParser());
+// // Set security HTTP headers
+// app.use(helmet());
+
+
+// // Limit requests from same API
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: 'Too many requests from this IP, please try again in an hour!'
+// });
+// app.use('/api', limiter);
+
+// // Body parser, reading data from body into req.body
+// app.use(express.json({ limit: '10kb' }));
+// app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+// app.use(cookieParser());
+
+// // Data sanitization against NoSQL query injection
+// app.use(mongoSanitize());
+
+// // Data sanitization against XSS
+// app.use(xss());
+
+// // Prevent parameter pollution
+// app.use(
+//   hpp({
+//     whitelist: [
+//       'duration',
+//       'ratingsQuantity',
+//       'ratingsAverage',
+//       'maxGroupSize',
+//       'difficulty',
+//       'price'
+//     ]
+//   })
+// );
+
+// // Test middleware
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   console.log(req.cookies);
+//   next();
+// });
+
+
+// // Connect to MongoDB using the environment variable
+// const dbURL = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+// mongoose
+//   .connect(dbURL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log('Connected to the database');
+//   })
+//   .catch((err) => {
+//     console.error('Error connecting to the database', err);
+//   });
+
+// // Define Routes
+// const AppError = require('./utils/appError');
+// const tourRoutes = require('./routes/tourRoutes');
+// const userRoutes = require('./routes/userRoutes');
+// const globalErrorHandler = require('./controllers/errorController');
+// // Use Routes
+// app.use('/api/tour', tourRoutes);
+// app.use('/api/users', userRoutes);
+
+// app.all('*', (req, res, next) => {
+//   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+// });
+
+// app.use(globalErrorHandler);
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}...`);
+// });
